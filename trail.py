3227df66191afbeefb10eea8cbc51e9faf9a55ad
Jayavardhan-7 <jayavardhanperala@gmail.com>
2024-12-06 19:31:34 +0530

    delete trail.py

--- trail.py
+++
@@ -1,4 +0,0 @@
-import google.generativeai as genai
-genai.configure(api_key="AIzaSyDd4kBxpk_wUcRsym7PugAyx9RR01VAbrQ")
-model=genai.GenerativeModel("gemini-1.5-flash")
-response=model.generate_content("What is java?")
